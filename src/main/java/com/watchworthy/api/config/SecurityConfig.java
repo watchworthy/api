cfb4a9ded38b411214059b972e6bcec1c9ab5b53
mirsadkr <mirsaddkr@gmail.com>
2023-04-30 23:51:30 +0200

    add spring-security + cors configuration

---
+++ src/main/java/com/watchworthy/api/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.watchworthy.api.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()
+                .authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList(RequestMethod.GET.name(),
+                RequestMethod.POST.name(),
+                RequestMethod.OPTIONS.name(),
+                RequestMethod.DELETE.name(),
+                RequestMethod.PUT.name()));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
