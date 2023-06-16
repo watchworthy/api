b7d75e34946f48a7a5e68f240699a5af4b563043
Mirsad Krasniqi <mirsaddkr@gmail.com>
2023-06-16 21:59:13 +0200

    WAT-83: add swagger (#50)

--- src/main/java/com/watchworthy/api/config/AppConfig.java
+++ src/main/java/com/watchworthy/api/config/AppConfig.java
@@ -1,6 +1,10 @@
 package com.watchworthy.api.config;
 
 import com.watchworthy.api.repository.UserRepository;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +21,20 @@ public class AppConfig {
     public UserDetailsService userDetailsService() {
         return username ->  userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                );
     }
 
 }
